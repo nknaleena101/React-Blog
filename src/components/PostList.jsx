0b29b7ce4d9cc98d27f9fada25fd222d7da2ed63
Naleena <nknaleena101@gmail.com>
2025-01-01 15:57:53 +0530

    features page

---
+++ src/components/PostList.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Card from "./Card/Card";
+
+function PostList(props) {
+  return (
+    <>
+      {props.posts.map((post) => (
+        <Card 
+            key={post.pid}
+            id={post.id}
+            image={post.image}
+            title={post.title}
+            description={post.description}
+            category={post.category}
+            publishDate={post.publishDate}
+            blog={post.blog}
+        />
+      ))}
+    </>
+  );
+}
+
+export default PostList;
